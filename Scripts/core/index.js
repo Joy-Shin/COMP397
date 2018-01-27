fbe3842f8a93033e38572ec9c60e637104e8f7f4
Jowon Shin <joy.shin.2213@gmail.com>
2018-01-27 14:46:32 -0500

    Initial Commit

---
+++ Scripts/core/index.js
@@ -0,0 +1,24 @@
+// IIFE -- Immediately Invoked FUnction Expression
+// Encapsulate variables inside the function; not global variables
+(function(){
+
+function Init() {
+    console.log("Game Started")
+}
+
+function Start() {
+
+}
+
+function Update() {
+
+}
+
+functino Main() {
+
+}
+
+window.onload = Init; // no brackets here. Init() is event handler of window object
+
+
+})();
